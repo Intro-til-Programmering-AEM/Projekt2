d09191e303ea7463b3cdb3ee351f4ac0f28ed606
Wedenborg <emiliewedenborg@gmail.com>
2017-06-16 15:40:55 +0200

    Menu implementering

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
+from menu_helpers import *
+from systems import *
+from turtles import *
+import sys
 
+System = None
+N = None
+print("Welcome to the Lindenmayer system playground! :D")
+while True:
+    print("Your options:")
+    option = menu(main_options)
+    # Empty input restarts menu
+    if option is not None:
+        if option == 1:
+            print("Which system would you like to see?")
+            system_choice = menu(system_options)
+            if System is not None:
+                System = list(names.keys())[system_choice-1]
+            print("Please choose the desired number of iterations:")
+            n_choice = input_nonNeg_int("N = ")
+            if n_choice is not None:
+                N = n_choice
+        elif option == 2:
+            if System is not None and N is not None:
+                turtlePlot(turtleGraph(LindIter(System, N)))
+            else:
+                print("Please choose your desired L-system and number of iterations first")
+        elif option == 3:
+            print("Thank you for using the Lindenmayer system playground")
+            # Exit properly
+            sys.exit()
 
---
+++ menu_helpers.py
@@ -0,0 +1,51 @@
+#Menu haandtering projekt 2
+
+main_options = ["Choose the type of Lindenmayer system and the number of iterations", "Generate plots", "Quit"]
+system_options = ["Kochs curve", "Sierpinski triangle"]
+
+# Denne funktion tager et brugerinput (options) og sender det videre, hvis det er valid.
+# Hvis ikke så kommer der en fejlmeddelelse
+def input_option(options):
+    while(True):
+        try:
+            # Get number that may be a legal option
+            x = int(input_wrapper("Select an option: "))
+            # Check if it's legal
+            if x <= len(options) and x > 0:
+                return x
+            # if the options is not legal, an appropriate message is printed
+            else:
+                print("Not an option, please try again.")
+        # if no option is selected, return None
+        except EOFError:
+            return None
+        # if a wrong type of options is selected, pass and repeat while-statement
+        except ValueError:
+            print("Please input a number corresponding to the option you want to select.")
+            pass
+
+def menu(options):
+    # Print options with option numbers
+    for i in range(len(options)):
+        print(str(i+1)+". "+options[i]+".")
+    return input_option(options)
+
+# Denne funktion ser om der er et input fra brugeren og laver en EOFError, hvis input er tomt.
+# Den returnerer en string
+def input_wrapper(request):
+    x = input(request)
+    if x == "":
+        raise EOFError
+    return x
+
+def input_nonNeg_int(request):
+    try:
+        x = int(input_wrapper(request))
+        if x >= 0:
+            return x
+        else:
+            raise ValueError
+    except ValueError:
+        print("Please input a non-negative integer")
+    except EOFError:
+        return None
