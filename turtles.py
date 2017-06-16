7eeb3320a3e835ada043e19527409b6a710ff24c
Wedenborg <emiliewedenborg@gmail.com>
2017-06-16 13:41:47 +0200

    IT WORKS!!!!!!

--- turtles.py
+++ turtles.py
@@ -7,20 +7,27 @@ def turtlePlot(turtleCommands):
     x=np.array([0,0])
     # Takes every second value starting from the second
     angles=turtleCommands[1::2]
-    np.insert(angles, 0, 0) # (index, value)
+    # Extra value inserted due to range in for loop
+    angles=np.insert(angles, 0, 0) # (index, value)
     steps=turtleCommands[::2]
+    steps=np.insert(steps, 0, 0) # (index, value)
+    # Same number of d-values as number of angles
     dvalues = [0 for _ in angles]
+    # Startvalue
     dvalues[0]=(np.array([1,0]))
     xvalues = [0 for _ in angles]
     xvalues[0]=(np.array([0,0]))
-    for i in range(1, len(angles)):
+    # Calculating d- and x-values
+    #Output = vector of coordinates
+    for i in range(1,len(angles)):
         dvalues[i] = np.dot(np.array([[ m.cos(angles[i]), -m.sin(angles[i])],
                [m.sin(angles[i]), m.cos(angles[i])]]),dvalues[i-1])
         xvalues[i] = xvalues[i-1]+steps[i]*dvalues[i]
     print(xvalues)
-
+    #Unpacks vector of datasets
     plt.plot(*zip(*xvalues)) # Plot line graph of x and y
-    plt.title("Simple line graph replica")
+    #Plottols
+    plt.title("Din mor") #TODO
     # Set the title of the graph
     plt.xlabel("x-values") # Set the x-axis label
     plt.ylabel("y-values") # Set the y-axis label
