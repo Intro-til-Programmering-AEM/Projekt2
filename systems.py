4450554386780c9bafb5ff6dba7390dde35df9a6
Marcus Medom Ryding <mrok4a@gmail.com>
2017-06-16 11:05:01 +0200

    Added error checking to L-system iteration
    Iterator uses unsafe version for performance reasons

--- systems.py
+++ systems.py
@@ -1,6 +1,13 @@
-def step(system, string):
-    vars, consts, dict, _ = system
+def step_unsafe(system, string):
+    _, consts, dict, _ = system
     return "".join(map(lambda s: str(s) if s in consts else dict[s], string))
+
+def step(system, string):
+    vars, consts, _, _ = system
+    symbols = vars + consts
+    if not all(c in symbols for c in string):
+        raise AssertionError("Lindenmayer string contains illegal symbols")
+    return step_unsafe(system, string)
 
 koch_dict = {
     'S': "SLSRSLS",
@@ -24,5 +31,5 @@ def LindIter(System, N):
     system = names[System]
     string = system[3]
     for _ in range(N):
-        string = step(system, string)
+        string = step_unsafe(system, string)
     return string
