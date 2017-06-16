8606b386e730ac1d5d982ab1355e4183a226a7ff
Wedenborg <emiliewedenborg@gmail.com>
2017-06-16 11:11:45 +0200

    Forsøg på noget godt

--- turtles.py
+++ turtles.py
@@ -1,6 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun 16 10:39:05 2017
+import numpy as np
+import math as m
+turtleCommands=np.array([1,0.5,1.5,0.2,1])
 
-@author: Emilie
-"""
+def turtlePlot(turtleCommands):
+    x=np.array([0,0])
+    d=np.array([1,0])
+    # Takes every second value starting from the second
+    angles=turtleCommands[1::2]
+    steps=turtleCommands[::2]
+    for i in range(len(angles)):
+        d=np.dot(np.array([[m.cos(angles[i]),-m.sin(angles[i])],
+                            [m.sin(angles[i]),m.cos(angles[i])]]),d)
+        print(d)
