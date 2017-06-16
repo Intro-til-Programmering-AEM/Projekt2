4cdbdbeb5bcf83f645fc6f5bb8a8a52ee379202c
Marcus Medom Ryding <mrok4a@gmail.com>
2017-06-16 10:57:34 +0200

    Transcribed Koch and Sierpinski L-systems

--- systems.py
+++ systems.py
@@ -1,3 +1,17 @@
 def step(system, string):
     vars, consts, dict, _ = system
     return "".join(map(lambda s: str(s) if s in consts else dict[s], string))
+
+koch_dict = {
+    'S': "SLSRSLS",
+}
+
+koch = (['S'], ['L','R'], koch_dict, "S")
+
+sierpinski_dict = {
+    'A': "BRARB",
+    'B': "ALBLA",
+}
+
+sierpinski = (['A','B'], ['L','R'], sierpinski_dict, "A")
+
