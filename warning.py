b9238b31b4f437a7ff7c0d39f2f2ce3f0fda40eb
Andreas Egelund Mortensen <andreas.egelund@live.dk>
2017-06-19 13:25:54 +0200

    warning comments 1

--- warning.py
+++ warning.py
@@ -1,9 +1,16 @@
 #Warning
 import math as m
 
+# A function that returns True if the string is at risk of exceeding some
+# preset threshold
 def shouldWarn(system,N):
+    # The largest value the program advises the user to compute
+    threshold = 7000
+    # Unpack the system for easy access to each tuple
     variables, constants, rules, initialString = system
+    # Make a list of occurences of variables produced by each variable when taking one step
     num_vars = [sum(string.count(v) for v in variables) for string in rules.values()]
+    # Finds the highest
     var_maks=max(num_vars)
-    return N > m.log(7000,var_maks) #For bad computer
+    return N > m.log(threshold,var_maks) #For bad computer
 
