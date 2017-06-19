bdf0b18d609c76025e4157631434828618041a12
Wedenborg <emiliewedenborg@gmail.com>
2017-06-19 13:23:16 +0200

    more comments

--- warning.py
+++ warning.py
@@ -2,8 +2,9 @@
 import math as m
 
 def shouldWarn(system,N):
+    return N > m.log(7000,max_vars(system)) #For bad computer
+
+def max_vars(system):
     variables, constants, rules, initialString = system
     num_vars = [sum(string.count(v) for v in variables) for string in rules.values()]
-    var_maks=max(num_vars)
-    return N > m.log(7000,var_maks) #For bad computer
-
+    return max(num_vars)
