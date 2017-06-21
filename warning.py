62202e7523ef931123e93aef82e49e7694a38de7
Marcus Medom Ryding <mrok4a@gmail.com>
2017-06-21 15:22:07 +0200

    My hands are typing words

--- warning.py
+++ warning.py
@@ -1,12 +1,15 @@
 import math as m
 from systems import max_vars
 
-# Returns True if the length of the string is at risk of
+# Returns True if the length of the string
+# that would be generated if the system `system`
+# were run for `N` iterations is at risk of
 # exceeding a predefined, empirically determined threshold
 # (roughly as long as we were willing to wait for our slowest computer to run)
 def shouldWarn(system,N):
-    # The largest value the program advises the user to compute
     threshold = 2**18
+    # Upper bound on number of variables a single variable
+    # can give rise to when expanded
     bound = max_vars(system)
     # If no variables expand to a string containing multiple variables,
     # the string size won't grow exponentially,
