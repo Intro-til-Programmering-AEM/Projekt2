aa023188e969923f4c914d72d301ba950664dd69
Andreas Egelund Mortensen <andreas.egelund@live.dk>
2017-06-21 13:26:10 +0200

    Through comments menu_helpers

--- menu_helpers.py
+++ menu_helpers.py
@@ -6,7 +6,7 @@ def menu(options):
         print(str(i+1)+". "+options[i]+".")
     return input_option(options)
 
-# This function aks for a userinput until an input is given and returns it if the input is valid
+# This function asks for a userinput until an input is given and returns it if the input is valid
 # If the input is not valid an errormessage is printet
 def input_option(options):
     while(True):
@@ -48,36 +48,59 @@ Cancelling at any point will return you to the main menu.
 Since an empty input is used to cancel, use a single space to indicate an empty string.
 All characters in your strings will be counted, so if you include e.g. a comma or a space, it will be counted as a symbol.
 Each character will be counted only once.""")
+    # Put 'L' and 'R' in a predefined list. Used to deny the user possibility
+    # of overwriting them later
     predefined = ['L', 'R']
+    # The user chooses constants. Can't be the predefined constants.
     constant_string = input_legal_string("containing all your constants", lambda c: c not in predefined)
     if constant_string is None:
         return None
     else:
+        # Put chosen constants into a list
         constants = list(set(constant_string))
+    # The user chooses variables. Can't be predefined constants or constants.
     variable_string = input_legal_string("containing all your variables", lambda c: c not in constants+predefined, "Symbols cannot be both constants and variables.")
     if variable_string is None:
         return None
     else:
+        # Put chosen variables into a list
         variables = list(set(variable_string))
+    # Put both variables and constants into symbols
     symbols = constants+variables
+    # The user chooses which constants and/or variables that represent line segments
+    # Only chosen constants and variables can represent line segments.
     line_string = input_legal_string("containing all the symbols that should represent line segments", lambda c: c in symbols)
     if line_string is None:
         return None
     else:
+        # Put line symbols into a list
         line_symbols = list(set(line_string))
+    # The user chooses the initial string. Only chosen constants, variables and
+    # predefined constants may be in the initial string
     initial_string = input_legal_string("that your system will be iterated from", lambda c: c in symbols+predefined)
     if initial_string is None:
         return None
+    # Create an empty list to be filled out with replacements for the variables
     rules = []
+    # The replacements must be constants, variables or predefined constants
     for c in variable_string:
         rule_string = input_legal_string("which "+str(c)+" should be replaced with", lambda c: c in symbols+predefined)
         if rule_string is None:
             return None
+        # Fill out the rules string
         rules.append((c, rule_string))
+    # All the information of this new-born system is returned in the same
+    # format as the predefined systems.
     return (constants+predefined, initial_string, dict(rules), line_symbols)
 
+# This function asks the user to input a string according to a request, which
+# lets the user know what kind of string is expected as input.
+# It can also tell if a given string is legal - that is, the string must pass
+# a test defined in the test-variable. If the test is not passed, an appropriate
+# error message will be shown and the user will get to try again.
 def input_legal_string(request, test, error = "String must only contain legal symbols."):
     while True:
+        # Return None if x is an empty string. Otherwise x is the input of the user
         try:
             x = input_wrapper("Please input a string "+request+": ")
         except EOFError:
@@ -85,14 +108,18 @@ def input_legal_string(request, test, error = "String must only contain legal sy
         if x == " ":
             # Deliberately return the empty string if input is a single space
             return ""
+        # Print an error message if a non-allowed input is given
         if not all(test(c) for c in x):
             print(error)
+        # Check if 'L' and 'R' are allowed, and check if they are in the string
+        # An error is printed if L and R are not allowed but present
             if all(not test(c) for c in "LR") and any(c in "LR" for c in x):
                 print("Remember, you can't define L and R yourself, and they represent 45° turns.")
+        # Return x if none of the above problems were encountered
         else:
             return x
 
-def input_nonNeg_int(request):
+def input_int_constrained(request, test, error):
     try:
         x = int(input_wrapper(request))
         if x >= 0:
@@ -103,3 +130,4 @@ def input_nonNeg_int(request):
         print("Please input a non-negative integer")
     except EOFError:
         return None
+
